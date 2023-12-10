ee7b0ef5e816467ebfe2c8c100c10a8c29500638
Matheus Henrique <matheushenriquep285@gmail.com>
2023-12-10 15:52:34 -0300

    feat: Problema 174 (WA)

---
+++ 174/174.py
@@ -0,0 +1,24 @@
+import math
+
+
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        # Pegando qtd de linhas e colunas.
+        linhas = len(dungeon)-1
+        colunas = len(dungeon[0])-1
+
+        M = [[math.inf for _ in range(colunas)] for _ in range(linhas)] # Incializando a matriz de custos.
+        # Marcando os menores valores para a pos. terminal.
+        M[linhas][colunas-1] = 1
+        M[linhas-1][colunas] = 1
+
+        for i in reversed(range(linhas)):
+            for j in reversed(range(colunas)):
+                menorVida = min(M[i+1][j], M[i][j+1]) - dungeon[i][j]
+                if menorVida<1:
+                    M[i][j] = 1
+                else:
+                    M[i][j] = menorVida
+
+        
+        return M[0][0]
